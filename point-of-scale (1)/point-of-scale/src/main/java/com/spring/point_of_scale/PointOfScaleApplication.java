5d70d0221074f8a136a527c404ab20cf1bdb6488
Manujapabodhana <rpmanuja123@gmail.com>
2025-11-02 21:03:15 +0530

    hhh

---
+++ point-of-scale (1)/point-of-scale/src/main/java/com/spring/point_of_scale/PointOfScaleApplication.java
@@ -0,0 +1,13 @@
+package com.spring.point_of_scale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointOfScaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PointOfScaleApplication.class, args);
+	}
+
+}
---
+++ point-of-scale (1)/point-of-scale/src/test/java/com/spring/point_of_scale/PointOfScaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.point_of_scale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointOfScaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
