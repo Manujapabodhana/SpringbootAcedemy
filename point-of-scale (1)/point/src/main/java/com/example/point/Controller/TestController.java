85ce80d0c2dbdd37de816b9c5f7ca5d13cd1f13c
Manujapabodhana <rpmanuja123@gmail.com>
2025-11-03 08:03:41 +0530

    s

---
+++ point-of-scale (1)/point/src/main/java/com/example/point/Controller/TestController.java
@@ -0,0 +1,21 @@
+package com.example.point.Controller;
+
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("api/v1/test")
+
+public class TestController {
+
+    @GetMapping
+    public  void getMyText() {
+        String myText= "this is my first springboot app";
+        System.out.println(myText);
+
+    }
+}
